5516af8ebe9497df778cd22a98596fd6357b151d
sebtu <studzinski1988@protonmail.com>
2024-06-30 20:28:57 +0200

    part3-deploy-static-nextjs-site-on-github-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/nextjs-and-mdx-blog',
+  assetPrefix: 'https://sebtu.github.io/nextjs-and-mdx-blog/',
   output: "export"
 };
 
