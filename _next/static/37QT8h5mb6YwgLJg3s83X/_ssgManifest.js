fa6924cc1d347cd02bb6f384f3a0509a99125282
sebtu <sebtu@users.noreply.github.com>
2024-06-30 18:38:19 +0000

    Deploying to gh_pages from @ sebtu/nextjs-and-mdx-blog@7209658a16f9e72c4b4cd94029971fced1f7c786 🚀

---
+++ _next/static/37QT8h5mb6YwgLJg3s83X/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fblog\u002Fpage\u002F[pageNum]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/layout-697954f49e568dda.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{829:function(n,e,t){Promise.resolve().then(t.t.bind(t,8877,23)),Promise.resolve().then(t.t.bind(t,231,23))},8877:function(){}},function(n){n.O(0,[601,231,971,23,744],function(){return n(n.s=829)}),_N_E=n.O()}]);
